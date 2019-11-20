fe77a80e48ba1f20fa82fe7dbf4acc0538be2109
João Pedro <joao@winker.com>
2019-11-20 01:52:25 -0300

    tarefas de build do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,29 @@ const ts    = require('gulp-typescript');
 /** Cria um projeto virtual lendo o arquivo de configuraçao do projeto*/
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', () => {
+gulp.task('scripts', ['static'], () => { // depende de static
     const tsResult = tsProject.src() // Aqui está pego o codigo fonte do src typescript
         .pipe(tsProject()); //a  função tsProject passada para o pipe vai pegar o fonte da chamada anterior e compilar o código
 
     return tsResult.js // javascript gerado pelo tsProject
         .pipe(gulp.dest('dist')); // diretorio onde será gerado o arquivo
 });
+
+gulp.task('static', ['clean'], () => { // depende de clean
+    return gulp.src(['src/**/*.json']) // todos os arquivos fontes no formato json serão copiados para dist
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', () => {
+    return gulp.src('dist') // sempre que um arquivo for removido no src ele será removido no dist
+        .pipe(clean());
+});
+
+// gulp.task('build', ['clean', 'static', 'scripts']);
+gulp.task('build', ['scripts']); // como o scripts depende do static e do clean eles serão executados, não precisa explicitá-los
+
+gulp.task('watch', ['build'], () => {
+    return gulp.watch(['src/**/*.ts', 'src/**/*.json'], ['build']); // toda a vez que houver uma mudança em um ts ou json ele irá executar a tarefa build
+});
+
+gulp.task('default', ['watch']); // tarefa que será executada ao utilizar o gulp sem nenhum argumento
