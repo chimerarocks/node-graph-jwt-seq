d5709b02923aee183e9833ef51e5027d831899b9
João Pedro <joao@winker.com>
2019-11-21 01:44:09 -0300

    dbconnection interface

---
+++ src/interfaces/DbConnectionInterface.ts
@@ -0,0 +1,8 @@
+import * as Sequelize from "sequelize";
+import {ModelsInterface} from "./ModelsInterface";
+
+export interface DbConnectionInterface extends ModelsInterface {
+
+  sequelize: Sequelize.Sequelize
+
+}
