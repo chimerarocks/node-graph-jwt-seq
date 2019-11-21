13178ad97def306f9eec1a693e611ff07300b08a
João Pedro <joao@winker.com>
2019-11-21 03:02:56 -0300

    UserModel

---
+++ src/models/UserModel.ts
@@ -0,0 +1,68 @@
+import * as Sequelize from "sequelize";
+import {BaseModelInterface} from "../interfaces/BaseModelInterface";
+import {compareSync, genSaltSync, hashSync} from "bcryptjs";
+
+export interface UserAttributes {
+  id?: number;
+  name?: string;
+  email?: string;
+  password?: string;
+  photo?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface UserInstance extends Sequelize.Instance<UserAttributes>, UserAttributes {
+  isPassword(encodedPassword: string, password: string): boolean;
+}
+
+export interface UserModel extends BaseModelInterface, Sequelize.Model<UserInstance, UserAttributes> {}
+
+export default (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes): UserModel => {
+
+  const User: UserModel = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+      validate: {
+        notEmpty: true // por mais que nao permita null tambem nao pode permitir vazio
+      }
+    },
+    photo: {
+      type: DataTypes.BLOB({ // será codificada para base64 ao ser cadastrada no banco de dados
+        length: 'long'
+      }),
+      allowNull: true,
+      defaultValue: null
+    }
+  }, {
+    tableName: 'users', // por padrao o nome da tabela é o nome do model no plural, mas nesse caso como a primeira letra tb vai ser minuscula tem que especificar
+    hooks: { //são como as triggers do mysql, mas sáo conhecidos como lifecycle hooks no sequelize
+      beforeCreate: (user: UserInstance, options: Sequelize.CreateOptions): void => {
+        const salt = genSaltSync();
+        user.password = hashSync(user.password, salt);
+      }
+    }
+  });
+
+  User.prototype.isPassword = (encodedPassword: string, password: string): boolean => {
+      return compareSync(password, encodedPassword);
+  };
+
+  return User;
+}
