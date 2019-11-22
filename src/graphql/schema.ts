31cd15838feeea6408eb95f0d154539b90fe82bc
João Pedro <joao@winker.com>
2019-11-22 02:30:17 -0300

    User schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -2,6 +2,8 @@ import {makeExecutableSchema} from "graphql-tools";
 import {Query} from "./query";
 import {Mutation} from "./mutation";
 
+import {userTypes} from "./resources/user/user.schema";
+
 const SchemaDefinition =  `
   type Schema {
     query: Query
@@ -13,7 +15,8 @@ export default makeExecutableSchema({
   typeDefs: [
     SchemaDefinition,
     Query,
-    Mutation
+    Mutation,
+    userTypes
   ]
 });
 
